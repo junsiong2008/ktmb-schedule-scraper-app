05621ee79350a0f4be08008854c2561aa6444a99
junsiong <=>
2026-01-01 17:46:05 +0800

    Removed script used for verification purposes

--- ktmb_api/verify_time.js
+++
@@ -1,5 +0,0 @@
-const { getCurrentTimeHHMMSS, getCurrentDate, getCurrentDayName } = require('./src/utils/timeUtils');
-
-console.log('Current Time (MYT):', getCurrentTimeHHMMSS());
-console.log('Current Date (MYT):', getCurrentDate());
-console.log('Current Day (MYT):', getCurrentDayName());
